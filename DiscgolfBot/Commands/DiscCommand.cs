39c7dd0a48bd517356bf3b6b227f5cd3a447eaac
Tristan Goetz <tgoetz@northstar.io>
2023-11-16 00:16:43 -0500

    Initial Commit

---
+++ DiscgolfBot/Commands/DiscCommand.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext;
+
+namespace DiscgolfBot.Commands
+{
+    public class DiscCommand : BaseCommandModule
+    {
+        [Command("disc")]
+        public async Task Command(CommandContext ctx)
+        {
+            Console.WriteLine("Greet Called");
+            await ctx.Channel.SendMessageAsync("Greetings! Thank you for executing me!");
+        }
+    }
+}
---
+++ DiscgolfBot/Program.cs
@@ -0,0 +1,76 @@
+﻿/* This is the cancellation token we'll use to end the bot if needed(used for most async stuff). */
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+CancellationTokenSource _cts;
+DiscordClient _discord;
+
+try
+{
+    Console.WriteLine("[info] Welcome to my bot!");
+    _cts = new CancellationTokenSource();
+
+    // Load the config file(we'll create this shortly)
+    Console.WriteLine("[info] Loading config file..");
+    var _config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.Development.json", optional: true)
+        .Build();
+    
+    // Create the DSharpPlus client
+    Console.WriteLine("[info] Creating discord client..");
+    _discord = new DiscordClient(new DiscordConfiguration
+    {
+        Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+        Token = _config.GetValue<string>("discord:token"),
+        TokenType = TokenType.Bot
+    });
+
+    // Create the interactivity module(I'll show you how to use this later on)
+    //_discord.UseInteractivity(new InteractivityConfiguration()
+    //{
+    //    PollBehaviour = PollBehaviour.KeepEmojis,
+    //    Timeout = TimeSpan.FromSeconds(30)
+    //});
+
+    var prefixConfiguration = _config.GetValue<string>("discord:commandPrefix") ?? 
+        throw new InvalidDataException("No Discord:commandPrefix value");
+
+    var prefixes = prefixConfiguration.ToCharArray().Select(c => $"{c}");
+    Console.WriteLine($"[info] Command prefixes: {string.Join(',', prefixes)}");
+
+    // Build dependancies and then create the commands module.
+    var commands = _discord.UseCommandsNext(new CommandsNextConfiguration
+    {
+        StringPrefixes = prefixes, // Load the command prefix(what comes before the command, eg "!" or "/") from our config file
+    });
+
+    // Add command loading
+    Console.WriteLine("[info] Loading command modules..");
+
+    commands.RegisterCommands(Assembly.GetExecutingAssembly());
+
+    Console.WriteLine($"[info] {commands.RegisteredCommands.Count} command modules loaded");
+
+    RunAsync().Wait();
+}
+catch (Exception ex)
+{
+    // This will catch any exceptions that occur during the operation/setup of your bot.
+    Console.Error.WriteLine(ex.ToString());
+}
+
+async Task RunAsync()
+{
+    // Connect to discord's service
+    Console.WriteLine("Connecting..");
+    await _discord.ConnectAsync();
+    Console.WriteLine("Connected!");
+
+    // Keep the bot running until the cancellation token requests we stop
+    while (!_cts.IsCancellationRequested)
+        await Task.Delay(TimeSpan.FromMinutes(1));
+}
