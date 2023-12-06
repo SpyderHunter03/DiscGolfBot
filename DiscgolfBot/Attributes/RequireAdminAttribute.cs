75cc82b873a65a177f326b0baf4eac9ed6c328f6
Tristan Goetz <tgoetz@northstar.io>
2023-12-05 21:45:56 -0500

    Update Admin Attribute

--- DiscgolfBot/Attributes/RequireAdminAttribute.cs
+++ DiscgolfBot/Attributes/RequireAdminAttribute.cs
@@ -1,4 +1,6 @@
 ﻿using DiscgolfBot.Data;
+using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,7 +13,14 @@ namespace DiscgolfBot.Attributes
             //return ctx.User.Id == 337045211362623493;
             var userRepository = ctx.Services.GetService<IUserRepository>();
             var isAdminUser = await userRepository!.IsAdminUser(ctx.User.Id);
-            
+
+            if (!isAdminUser)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent($"This command is for admins only.")
+                );
+            }
+
             return isAdminUser;
         }
     }
